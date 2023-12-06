8d4eca9bcfd6f237b951581ef99dfd35eab9d582
DanMark <daniel.mark.kiss@gmail.com>
2023-12-06 13:39:58 +0100

    feladat 2 dokumentálása

--- feladat02.py
+++ feladat02.py
@@ -1,47 +1,28 @@
 import pandas as pd
-from scipy.stats import chi2_contingency
+from scipy.stats import f_oneway
 
 
 def main():
-    """A program fő függvénye."""
-    # Adatok beolvasása
-    data = pd.read_csv("bead11.2.csv")
+    # Adatok beolvasása CSV-fájlból
+    df = pd.read_csv("bead11.2.csv")
 
-    osszehasonlitas(data, "Csoport 1", "Csoport 2")
-    osszehasonlitas(data, "Csoport 1", "Csoport 3")
-    osszehasonlitas(data, "Csoport 1", "Csoport 4")
-    osszehasonlitas(data, "Csoport 2", "Csoport 3")
-    osszehasonlitas(data, "Csoport 2", "Csoport 4")
-    osszehasonlitas(data, "Csoport 3", "Csoport 4")
+    # ANOVA teszt végrehajtása
+    stat, p_value = f_oneway(
+        df["Csoport 1"], df["Csoport 2"], df["Csoport 3"], df["Csoport 4"]
+    )
 
-    # Chi-négyzet teszt
-
-
-def osszehasonlitas(data, param1, param2):
-    contingency_table = pd.crosstab(data[param1], data[param2], margins=False)
-    chi2, p, _, _ = chi2_contingency(contingency_table)
-
-    # Eredmény kiírása
-    print(f"Chi-négyzet érték: {chi2}")
-    print(f"P-érték: {p}")
-
-    # Szignifikancia ellenőrzése
+    # Kiértékelés
     alpha = 0.05
-    if p < alpha:
+
+    print(f"Statisztika: {stat}, p-érték: {p_value}")
+
+    if p_value < alpha:
         print(
-            "Van szignifikáns különbség a csoportok között a mosolygós emojik használatában.("
-            + param1
-            + " "
-            + param2
-            + ")"
+            "Van szignifikáns különbség a csoportok között a mosolygós emojik használatában."
         )
     else:
         print(
-            "Nincs szignifikáns különbség a csoportok között a mosolygós emojik használatában.("
-            + param1
-            + " "
-            + param2
-            + ")"
+            "Nincs szignifikáns különbség a csoportok között a mosolygós emojik használatában."
         )
 
 
