c71de1e9cba8509fc55300d3340b116fe0b4bf3c
DanMark <daniel.mark.kiss@gmail.com>
2023-12-06 14:20:48 +0100

    feladat 3 dokumentálás

--- feladat03.py
+++ feladat03.py
@@ -2,37 +2,91 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+import statsmodels.api as sm
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 
 
 def main():
     """A program fő függvénye."""
-    # Adatok beolvasása
-    data = pd.read_csv("bead11.3.csv")
+    # CSV fájl beolvasása
+    file_path = "bead11.3.csv"
+    data = pd.read_csv(file_path)
     data["Év-hónap"] = pd.to_datetime(data["Év-hónap"])
     data.set_index("Év-hónap", inplace=True)
 
     # Idősor diagram
     plt.figure(figsize=(10, 6))
-    plt.plot(
-        data["Mosolygós emojik használata (darab)"], label="Mosolygós emojik használata"
-    )
-    plt.title("Mosolygós emojik használata az időben")
-    plt.xlabel("Dátum")
-    plt.ylabel("Emoji használat (darab)")
-    plt.legend()
+    plt.plot(data["Mosolygós emojik használata (darab)"], marker="o")
+    plt.title("Mosolygós emojik használata az évek során")
+    plt.xlabel("Év-hónap")
+    plt.ylabel("Darabszám")
+    plt.xticks(rotation=45)
+    plt.grid(True)
     plt.show()
 
-    # Autokorrelációk számolása
-    lag_acf = acf(data["Mosolygós emojik használata (darab)"], nlags=20)
-    lag_pacf = pacf(data["Mosolygós emojik használata (darab)"], nlags=20, method="ols")
+    # Tapasztalati autokorrelációs függvény (ACF)
+    plt.figure(figsize=(10, 6))
+    plot_acf(data["Mosolygós emojik használata (darab)"], lags=30)
+    plt.title("Tapasztalati autokorrelációs függvény (ACF)")
+    plt.xlabel("Lag")
+    plt.ylabel("Autokorreláció")
+    plt.show()
 
-    # Modellezés és előrejelzés
-    model = SARIMAX(
-        data["Mosolygós emojik használata (darab)"],
-        order=(1, 1, 1),
-        seasonal_order=(1, 1, 1, 12),
+    # Parciális autokorrelációs függvény (PACF)
+    plt.figure(figsize=(10, 6))
+    plot_pacf(data["Mosolygós emojik használata (darab)"], lags=12)  # Módosított érték
+    plt.title("Parciális autokorrelációs függvény (PACF)")
+    plt.xlabel("Lag")
+    plt.ylabel("Parciális autokorreláció")
+    plt.show()
+
+    # Szezonális dekompozíció
+    result = sm.tsa.seasonal_decompose(
+        data["Mosolygós emojik használata (darab)"], model="additive", period=12
     )
-    results = model.fit(disp=-1)
+    trend = result.trend.dropna()
+    seasonal = result.seasonal.dropna()
+    residual = result.resid.dropna()
+
+    # Ábrázolás
+    plt.figure(figsize=(14, 10))
+
+    plt.subplot(4, 1, 1)
+    plt.plot(data["Mosolygós emojik használata (darab)"], label="Eredeti")
+    plt.legend()
+
+    plt.subplot(4, 1, 2)
+    plt.plot(trend, label="Trend")
+    plt.legend()
+
+    plt.subplot(4, 1, 3)
+    plt.plot(seasonal, label="Szezonális")
+    plt.legend()
+
+    plt.subplot(4, 1, 4)
+    plt.plot(residual, label="Reziduális")
+    plt.legend()
+
+    plt.show()
+
+    # ARIMA modell illesztése a reziduálisokra
+    order = (1, 1, 1)  # Példaérték, a modellrend megválasztása
+    model = sm.tsa.ARIMA(residual, order=order)
+    results = model.fit()
+
+    # Illesztés eredményeinek megjelenítése
+    print(results.summary())
+
+    # Illesztett értékek kiszámítása
+    fitted_values = results.fittedvalues
+
+    # Illesztett és eredeti értékek összehasonlítása
+    plt.figure(figsize=(10, 6))
+    plt.plot(residual, label="Eredeti reziduálisok")
+    plt.plot(fitted_values, color="red", label="Illesztett reziduálisok")
+    plt.legend()
+    plt.title("ARIMA Modell illesztése a reziduálisokra")
+    plt.show()
 
     # Előrejelzés a következő hónapokra
     forecast_steps = 12
